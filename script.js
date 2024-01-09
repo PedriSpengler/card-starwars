0fe8991b5ada9cecbcded9d75fc49b2ed4c07334
PedriSpengler <pedroj.oficial@gmail.com>
2024-01-09 13:41:20 -0400

    scale all

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ cardPrincipal.addEventListener('mouseleave', function() {
   cardPrincipal.style.transform = 'scale(1)';
 });
 
+var cardSecundario = document.querySelector('.card-secundario');
+
+cardSecundario.addEventListener('mouseenter', function(){
+  cardSecundario.style = 'z-index: 2';
+});
+
+cardSecundario.addEventListener('mouseleave', function(){
+  cardSecundario.style = 'z-index: -1';
+})
+
+var cardTerciario = document.querySelector('.card-terciario');
+
+cardTerciario.addEventListener('mouseenter', function(){
+  cardTerciario.style = 'z-index: 2';
+});
+
+cardTerciario.addEventListener('mouseleave', function(){
+  cardTerciario.style = 'z-index: -1';
+})
